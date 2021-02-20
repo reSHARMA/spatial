524e5b35377fb237700fdd5be231850419c1825b
Mehul Jain <mehul11jain@outlook.com>
2021-02-20 13:29:56 +0530

    Benchmark Runner Interface added

--- include/Benchmark/PTABenchmark.h
+++ include/Benchmark/PTABenchmark.h
@@ -1,85 +1,41 @@
-#ifndef PTA_BENCHMARK_H_
-#define PTA_BENCHMARK_H_
+#ifndef PTA_BENCHMARK_H
+#define PTA_BENCHMARK_H
+
+#include "set"
+#include "string"
+#include "llvm/IR/Argument.h"
 #include "Benchmark.h"
-#include "llvm/IR/Metadata.h"
-#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
 
 namespace spatial {
 
-enum Soundness {
-  Sound,
-  Unsound,
-};
-class PTABenchmarkRunner : public BenchmarkRunner {
-  Soundness SoundOrUnsound;
-  std::map<llvm::Instruction *, std::vector<std::string>> TCResult;
-  std::map<llvm::BasicBlock *, std::vector<int>> PrecisionResult;
+class PTABenchmarkRunner : public BenchmarkRunner{
+private:  
+  std::map<llvm::Instruction *, std::string> Result;
+  void evaluateUtil(llvm::CallInst *, int, int, int);
 
 public:
-  PTABenchmarkRunner() : BenchmarkRunner(){    
-    SoundOrUnsound = Sound;
-  }
+  PTABenchmarkRunner() : BenchmarkRunner("ALIAS"){};
   std::vector<llvm::Value *> extract(llvm::Instruction *Inst);
   friend std::ostream &operator<<(std::ostream &OS, const PTABenchmarkRunner &B);
   template <class Ty>
-  void evaluate(llvm::Instruction *Inst, std::set<Ty *>, Ty *);
-  spatial::Soundness CheckSoundness();
-  void evaluatePrecision(llvm::Function &);
+  void evaluate(llvm::Instruction *Inst, std::set<Ty *>, std::set<Ty *>);
 };
 
 template <class Ty>
 void PTABenchmarkRunner::evaluate(llvm::Instruction *I, std::set<Ty *> A,
-                                     Ty *B) {
+                               std::set<Ty *> B) {
   llvm::CallInst *Inst = llvm::cast<llvm::CallInst>(I);
-  std::string Status, Expected;
-  llvm::StringRef FunctionName = Inst->getCalledFunction()->getName();
-
-  llvm::MDNode *md = I->getMetadata("dbg");
-  int lineno = llvm::cast<llvm::DILocation>(md)->getLine();
-
-  // std::cout << lineno << "\n";
-
-  if (FunctionName.contains("May"))
-    Expected = "MayPointsTo";
-  else if (FunctionName.contains("Must"))
-    Expected = "MustPointsTo";
-  else if (FunctionName.contains("DoesNot"))
-    Expected = "DoesNotPointsTo";
-
-  if (Expected == "MayPointsTo") {
-    Status = "Fail";
-    for (auto it = A.begin(); it != A.end(); it++) {
-      if (*it == B) {
-        Status = "Pass";
-        break;
-      }
-    }
-  } else if (Expected == "MustPointsTo") {
-    if (A.size() > 1 || A.size() == 0) {
-      Status = "Fail";
-    } else {
-      Status = "Pass";
-      for (auto it = A.begin(); it != A.end(); it++) {
-        if (*it != B) {
-          Status = "Fail";
-          break;
-        }
-      }
-    }
-  } else {
-    Status = "Pass";
-    for (auto it = A.begin(); it != A.end(); it++) {
-      if (*it == B) {
-        Status = "Fail";
-        break;
-      }
-    }
-  }
-  TCResult[Inst].push_back(Status);
-  TCResult[Inst].push_back(Expected);
-  TCResult[Inst].push_back(std::to_string(lineno));
+  std::set<Ty> Result;
+  for (auto a : A)
+    Result.insert(*a);
+  for (auto b : B)
+    Result.insert(*b);
+  evaluateUtil(Inst, A.size(), B.size(), Result.size());
 }
-
 } // namespace spatial
 
 #endif
--- lib/Benchmark/PTABenchmark.cpp
+++ lib/Benchmark/PTABenchmark.cpp
@@ -1,128 +1,65 @@
+#include "Benchmark/Benchmark.h"
 #include "Benchmark/PTABenchmark.h"
+#include "set"
+#include "string"
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Operator.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
-#include "llvm/Support/raw_ostream.h"
-#include "set"
-#include "string"
-#include <iomanip>
-#define CYANB "\033[1;46m"
-#define REDT "\033[1;91m"
-#define GREENT "\033[1;92m"
-#define YELLOWT "\033[1;93m"
-#define RST "\033[0;m"
 
 namespace spatial {
 
-std::string print_args(llvm::CallInst* ci){    
-    std::string TCResult;
-    int i=0;
-	while(i<ci->getNumArgOperands())
-	{
-		std::string arg = llvm::dyn_cast<llvm::Instruction>(ci->getArgOperand(i))->getOperand(0)->getName().str();		
-		TCResult += arg;			
-		i++;
-		if(i!=ci->getNumArgOperands()) TCResult += ", " ;		
-	}
-    return TCResult;
+std::ostream &operator<<(std::ostream &OS, const PTABenchmarkRunner &B) {
+  for (auto R : B.Result) {
+    OS << R.second << "\n";
+  }
 }
 
-spatial::Soundness PTABenchmarkRunner::CheckSoundness(){
-    for(auto i : TCResult){
-        if(i.second[0] == "Fail" && (i.second[1] == "MayPointsTo")){
-            SoundOrUnsound = Soundness::Unsound;
-            break;
-        }
-        else{
-            SoundOrUnsound = Soundness::Sound;
-        }
-    }
-    return SoundOrUnsound;
+void PTABenchmarkRunner::evaluateUtil(llvm::CallInst *Inst, int A, int B,
+                                   int Result) {
+  std::string Status, Expected, Got;
+  llvm::StringRef FunctionName = Inst->getCalledFunction()->getName();
+  if (FunctionName.contains("MAY"))
+    Expected = "may";
+  if (FunctionName.contains("MUST"))
+    Expected = "must";
+  if (FunctionName.contains("NO"))
+    Expected = "no";
+  if (A == 0 || B == 0 || 2 * A == Result)
+    Got = "no";
+  else if (A == Result)
+    Got = "must";
+  else
+    Got = "may";
+  if (Expected == Got || (Expected == "may" && Got == "must"))
+    Status = "Pass";
+  else
+    Status = "Fail";
+  this->Result[Inst] =
+      "[" + Status + "] Expected: " + Expected + " Got: " + Got;
 }
 
-std::ostream& operator<<(std::ostream& OS, const PTABenchmarkRunner& B) {
-    int i = 1;
-    std::string header = "*********************************************** BENCHMARK RESULTS *********************************************************",res;
-    OS << "\n__________________________________________________________________________________________________________________________\n";
-    OS << header + "\n";
-    OS << "---------------------------------------------------------------------------------------------------------------------------\n\n";    
-    for (auto R = B.TCResult.begin();R != B.TCResult.end();R++) {
-        if(R == B.TCResult.begin()){
-            OS << "\t\t=================================================================================\n";
+std::vector<llvm::Value *> PTABenchmarkRunner::extract(llvm::Instruction *Inst) {
+  std::vector<llvm::Value *> Tokens;
+  if (llvm::CallInst *CI = llvm::dyn_cast<llvm::CallInst>(Inst)) {
+    llvm::Function *CalledFunction = CI->getCalledFunction();
+    if (CalledFunction && CalledFunction->getName().contains(this->Key)) {
+      for (llvm::Value *CallArg : CI->args()) {
+        if (llvm::BitCastInst *BI =
+                llvm::dyn_cast<llvm::BitCastInst>(CallArg)) {
+          Tokens.push_back(BI->getOperand(0));
+        } else if (llvm::BitCastOperator *BN =
+                       llvm::dyn_cast<llvm::BitCastOperator>(CallArg)) {
+          Tokens.push_back(BN->getOperand(0));
+        } else {
+          Tokens.push_back(CallArg);
         }
-        llvm::CallInst* ci = llvm::dyn_cast<llvm::CallInst>((*R).first); 
-        OS << std::setw(15) << "\t\t|  TestCase " << i << ":\t|\t" 
-        << std::setw(10) << "Line:" << (*R).second[2] << "   " << (*R).second[1] <<"(" 
-        << print_args(ci) << ")\t" << std::setw(5) << "|\t" ;
-        res = (((*R).second[0] == "Pass") ? (GREENT + (*R).second[0] + RST ): (REDT + (*R).second[0] + RST)); 
-        OS << res;
-        OS <<  "\t|\n";        
-        OS << "\t\t=================================================================================\n";
-        i++;
+      }
     }
-    OS << "\n---------------------------------------------------------------------------------\n";
-    if(B.SoundOrUnsound == Soundness::Sound){
-        OS << "\nSoundness Check : Analysis is \033[1;92mSound\033[0;m\n";
-    }
-    else{
-        OS << "\nSoundness Check : Analysis is \033[1;91mUnsound\033[0;m\n";
-    }
-    OS << "\n---------------------------------------------------------------------------------\n";
-    OS << "Precision Evaluation :\n\n";  
-    OS << "\t\t=========================================================================================================\n";
-    OS << "\t\t|\tProgram Point\t|\t" <<  "Total Pointers incorrect\t" << "|\tTotal Failed Predicates\t\t|\n";
-    for(auto i : B.PrecisionResult){
-        if(i == *(B.PrecisionResult.begin())){
-            OS << "\t\t=========================================================================================================\n";
-        }
-        OS << "\t\t|\tOut [" << (i.first)->getName().str() << "]\t" << std::setw(1) << "|";        
-        OS << std::setw(14) << "\t" << i.second[0] << "\t"<< std::setw(13) << "\t|\t" << std::setw(14) << i.second[1] << std::setw(13) << "\t|\n";        
-        OS << "\t\t=========================================================================================================\n";
-    }
-    OS << "____________________________________________________________________________________________________________________________\n\n";
-    return OS;
+  }
+  return Tokens;
 }
 
-std::vector<llvm::Value*> PTABenchmarkRunner::extract(llvm::Instruction* Inst) {
-    std::vector<llvm::Value*> Tokens;    
-    if (llvm::CallInst* CI = llvm::dyn_cast<llvm::CallInst>(Inst)) {        
-        llvm::Function* CalledFunction = CI->getCalledFunction();        
-        if (CalledFunction && CalledFunction->getName().contains("PointsTo")) {            
-            for (llvm::Value* CallArg : CI->args()) {
-                if(CallArg != *(CI->args().begin()))
-                    Tokens.push_back(llvm::dyn_cast<llvm::Instruction>(CallArg)->getOperand(0));
-                else
-                    Tokens.push_back(CallArg);                
-            }
-        }
-    }
-    return Tokens;
-}
-void PTABenchmarkRunner::evaluatePrecision(llvm::Function& F){
-    for(llvm::BasicBlock& BB : F){        
-        std::map<llvm::Value*, bool> visited;
-        PrecisionResult[&BB] = std::vector<int>(2,0);
-        for(llvm::Instruction& I : BB){
-            if(llvm::CallInst* ci = llvm::dyn_cast<llvm::CallInst>(&I)){
-                if(ci->getCalledFunction()->getName().contains("DoesNotPointsTo")){
-                    std::vector<std::string> res = TCResult[&I];
-                    if(res[0] == "Fail"){
-                        // Format -- Pointer Count for which it fails, Total predicates it fails;
-                        llvm::Value* arg1 = llvm::dyn_cast<llvm::Instruction>(ci->getArgOperand(0))->getOperand(0);                    
-                        if(!visited[arg1]){
-                            PrecisionResult[&BB][0]++;    
-                            visited[arg1] = true;
-                        }                    
-                        PrecisionResult[&BB][1]++;
-                    }
-
-                }
-            }
-        }
-    }
-
-}
-}  // namespace spatial
+} // namespace spatial
