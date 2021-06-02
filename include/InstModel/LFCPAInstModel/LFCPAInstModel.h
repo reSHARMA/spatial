75db131d493d9941ce079897dfcb0179ce57da5a
Aditi <aditi.raste@gmail.com>
2021-06-02 08:06:34 +0530

    commit LFCPAInstModel

--- include/InstModel/LFCPAInstModel/LFCPAInstModel.h
+++ include/InstModel/LFCPAInstModel/LFCPAInstModel.h
@@ -37,9 +37,8 @@ public:
 
   std::vector<int> extractRedirections(llvm::GlobalVariable *);
   template <typename GEP> Token *handleGEPUtil(GEP *, Token *);
-  spatial::InstInfo* getInstInfo(llvm::Instruction *);
+  Token * handleGEPUtil_1(llvm::GEPOperator *, Token *);
   bool isInstSkip (llvm::Instruction *);
-  void printInstInfoMap();
   template <typename GOP> bool isStructFieldPointerTy(GOP *);
   Token* extractDummy(std::string);
 };
