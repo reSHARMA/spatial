57a68014ccde2cca9b044f7136cab8c3812ee86e
aditiraste <aditiraste@users.noreply.github.com>
2022-05-08 10:31:15 +0000

    deploy: d851a5550f88e771223b073a556787966a8eece5

--- search/enumvalues_1.js
+++ search/enumvalues_1.js
@@ -3,8 +3,9 @@ var searchData=
   ['isalloca_0',['isAlloca',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aa95c09e8fd7ca8bae12fdca133818de5d',1,'spatial']]],
   ['isarray_1',['isArray',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aad313d0d637d7da2fde0dc6ba7d3cc13e',1,'spatial']]],
   ['isfunarg_2',['isFunArg',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aae663b42311b90610b30e46e801572bd1',1,'spatial']]],
-  ['isicmpgepopd_3',['isIcmpGEPOpd',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aa4eed1c9ad2828e2ec38195551c3bb26f',1,'spatial']]],
-  ['isonegepindx_4',['isOneGEPIndx',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aad358d524c20bf24ea33466ce7508ba04',1,'spatial']]],
-  ['isopbitcast_5',['isOpBitcast',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aae983ba3533317786ded74f21a049674a',1,'spatial']]],
-  ['isphigepopd_6',['isPhiGEPOpd',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aaf001364a991835657f217e58552c65d9',1,'spatial']]]
+  ['isfunptr_3',['isFunPtr',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aa0a61ced5571ec3da151a9fbb7e463bc6',1,'spatial']]],
+  ['isicmpgepopd_4',['isIcmpGEPOpd',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aa4eed1c9ad2828e2ec38195551c3bb26f',1,'spatial']]],
+  ['isonegepindx_5',['isOneGEPIndx',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aad358d524c20bf24ea33466ce7508ba04',1,'spatial']]],
+  ['isopbitcast_6',['isOpBitcast',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aae983ba3533317786ded74f21a049674a',1,'spatial']]],
+  ['isphigepopd_7',['isPhiGEPOpd',['../namespacespatial.html#a6d56a98337ae4a2142e918e5d685522aaf001364a991835657f217e58552c65d9',1,'spatial']]]
 ];
